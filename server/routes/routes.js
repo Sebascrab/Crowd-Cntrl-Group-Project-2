5e33345c9bb2ff13b187fe079ae24498e465d082
TommyAlv <tommyalvarado2@gmail.com>
2022-09-22 20:29:53 -0600

    moved footer to absolute bottom

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -6,11 +6,11 @@ const userController = require('../controllers/userController');
 // Routes below:
 router.get('/', userController.view);
 router.post('/', userController.find);
-router.get('/adduser', userController.form);
-router.post('/adduser', userController.create);
-router.get('/edituser/:id', userController.edit);
-router.post('/edituser/:id', userController.update);
-router.get('/viewuser/:id', userController.viewall);
+router.get('/addcustomer', userController.form);
+router.post('/addcustomer', userController.create);
+router.get('/editcustomer/:id', userController.edit);
+router.post('/editcustomer/:id', userController.update);
+router.get('/viewcustomer/:id', userController.viewall);
 router.get('/:id',userController.delete);
   
 module.exports = router;
