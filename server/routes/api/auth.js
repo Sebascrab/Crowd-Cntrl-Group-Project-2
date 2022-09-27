58ffe670a632f4ede9125a3d863d51229c9b14e3
Abel Gonzales <abelgonzales425@gmail.com>
2022-09-26 18:46:31 -0600

    cleaned up code

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -2,18 +2,8 @@ const { userCreate, userLogin, userLogout } = require('../../controllers/authCon
 
 const router = require('express').Router();
 
-
-
 router.route('/').post(userCreate);
 router.route('/login').post(userLogin);
 router.route('/logout').post(userLogout);
-
-
-
-
-
-
-
-
 
 module.exports = router
