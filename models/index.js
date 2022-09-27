104b1d8a3c07d6a407e729a28644dfafcfcd431b
TommyAlv <tommyalvarado2@gmail.com>
2022-09-26 19:25:00 -0600

    added models

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const userRoutes = require('./user-routes');
+const clientRoutes = require('./client-routes');
+
+
+router.use('/users', userRoutes);
+router.use('/clients', clientRoutes);
+
+
+module.exports = router;
