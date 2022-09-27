dd88711a7c4a466a0344c2bdc19599fcf62da70c
TommyAlv <tommyalvarado2@gmail.com>
2022-09-26 21:31:47 -0600

    removed index.html innards and added to handlebars

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,17 +3,17 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const apiRoutes = require('./api')
+const authRoutes = require('./auth.js');
 router.use('/api', apiRoutes);
-
 
 // Routes below:
 router.get('/', userController.view);
 router.post('/', userController.find);
-router.get('/addcustomer', userController.form);
-router.post('/addcustomer', userController.create);
-router.get('/editcustomer/:id', userController.edit);
-router.post('/editcustomer/:id', userController.update);
-router.get('/viewcustomer/:id', userController.viewall);
+router.get('/new-customer', userController.form);
+router.post('/new-customer', userController.create);
+router.get('/edit-customer/:id', userController.edit);
+router.post('/edit-customer/:id', userController.update);
 router.get('/:id',userController.delete);
+router.use('/auth', authRoutes)
   
 module.exports = router;
