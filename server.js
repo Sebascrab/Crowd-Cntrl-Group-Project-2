81b815dbe1201c3e8edb4d4fa3f3c5f5ca108b4d
TommyAlv <tommyalvarado2@gmail.com>
2022-09-28 14:45:13 -0600

    modified lists and added css

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.static("public/img"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(passport.initialize());
 app.use(passport.session());
 
