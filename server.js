6d0ce371b9d61137c84d90fb4e7eff269fd47c35
TommyAlv <tommyalvarado2@gmail.com>
2022-09-27 21:20:10 -0600

    added image to index

--- server.js
+++ server.js
@@ -21,10 +21,12 @@ const PORT = process.env.PORT || 3001;
 app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(express.static("public/img"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 app.use(routes);
